2e5852e6a49dd65473b0a5962107993aa3a634a9
arjun.airtory <arjun12345krishna@gmail.com>
2023-09-30 07:07:00 +0530

    Client & Server : WIP

---
+++ app/script.js
@@ -0,0 +1,48 @@
+
+// Get all the elements
+let ceiling_light_1 = document.getElementById('ceiling_light_1');
+let ceiling_light_2 = document.getElementById('ceiling_light_2');
+
+let strip_light_1 = document.getElementById('strip_light_1');
+let strip_light_2 = document.getElementById('strip_light_2');
+
+let fan_1 = document.getElementById('fan_1');
+let fan_2 = document.getElementById('fan_2');
+let fan_3 = document.getElementById('fan_3');
+let fan_4 = document.getElementById('fan_4');
+let fan_5 = document.getElementById('fan_5');
+let fan_6 = document.getElementById('fan_6');
+
+
+// Connect to the server
+let ws = new WebSocket('ws://localhost:3000');
+
+
+// When the connection is open
+ws.onopen = () => {
+    console.log('Connected to the server');
+};
+
+
+// When we receive a message from the server
+ws.onmessage = (event) => {
+    let circuit = JSON.parse(event.data);
+    
+    ceiling_light_1.innerHTML = `${circuit.ceiling_light_1 ? 'ON' : 'OFF'}`;
+    ceiling_light_2.innerHTML = `${circuit.ceiling_light_2 ? 'ON' : 'OFF'}`;
+    strip_light_1.innerHTML = `${circuit.strip_light_1 ? 'ON' : 'OFF'}`;
+    strip_light_2.innerHTML = `${circuit.strip_light_2 ? 'ON' : 'OFF'}`;
+    fan_1.innerHTML = `${circuit.fan_1 ? 'ON' : 'OFF'}`;
+    fan_2.innerHTML = `${circuit.fan_2 ? 'ON' : 'OFF'}`;
+    fan_3.innerHTML = `${circuit.fan_3 ? 'ON' : 'OFF'}`;
+    fan_4.innerHTML = `${circuit.fan_4 ? 'ON' : 'OFF'}`;
+    fan_5.innerHTML = `${circuit.fan_5 ? 'ON' : 'OFF'}`;
+    fan_6.innerHTML = `${circuit.fan_6 ? 'ON' : 'OFF'}`;
+
+};
+
+
+// trigger the toggle action on the server
+let toggleState = (circuit) => {
+    ws.send(JSON.stringify({ action: 'toggle', circuit }));
+}
---
+++ server/server.js
@@ -0,0 +1,57 @@
+
+let WebSocket = require('ws');
+let http = require('http');
+let server = http.createServer();
+let wss = new WebSocket.Server({ server });
+
+let circuit = {
+    ceiling_light_1: false,
+    ceiling_light_2: false,
+    strip_light_1: false,
+    strip_light_2: false,
+    fan_1: false,
+    fan_2: false,
+    fan_3: false,
+    fan_4: false,
+    fan_5: false,
+    fan_6: false,   
+};
+
+wss.on('connection', (ws) => {
+    
+    console.log('A new client connected...');
+    
+    // Send the current circuit status to the newly connected client
+    ws.send(JSON.stringify(circuit));
+
+    ws.on('message', (message) => {
+        try {
+
+            let data = JSON.parse(message);
+            console.log('Received:', data);
+
+            if (data.action === 'toggle') {
+                circuit[data.circuit] = !circuit[data.circuit];
+
+                // Broadcast the new circuit status to all connected clients
+                wss.clients.forEach((client) => {
+                    if (client.readyState === WebSocket.OPEN) {
+                        client.send(JSON.stringify(circuit));
+                    }
+                });
+            }
+
+        } catch (error) {
+            console.error('Error processing message:', error);
+        }
+    });
+
+    // When the client closes the connection
+    ws.on('close', () => {
+        console.log('Someone disconnected...');
+    });
+});
+
+server.listen(3000, () => {
+    console.log(`Server started on port 3000`);
+});
